9556785d49ca1c7d171f4ffb9478699105656d4c
Ryan Brown <itsjustryanbrown@gmail.com>
2024-12-30 13:31:55 -0500

    I have added no javascript functionality to the accordion

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # accordionTest
 An accessible modern javascript accordion the best way I know how to make one.
+
+Updated 12/26/2024
+So I wanted to improve this using some new tricks I learned.
+-First I wanted to make it so this could be used without JavaScript and utilize the ::target pseudo class to change the style.
+-I also wanted to to change the a tags from when there is no Javascript present to be buttons when there is JavaScript present.
+
+I have achi
--- src/accordion.js
+++ src/accordion.js
@@ -1,3 +1,20 @@
+const htmlTag = document.querySelector("html");
+
+htmlTag.classList.add("js");
+
+const triggers = document.querySelectorAll(".accordion-trigger");
+
+for (trigger of triggers){
+    let triggerButton = document.createElement("button");
+    let triggerHeader = document.createElement("h2");
+    let headerContent = "Accordion Trigger";
+    triggerButton.classList.add("accordion-trigger");
+    triggerButton.setAttribute("aria-expanded", false);
+    triggerButton.appendChild(triggerHeader);
+    triggerHeader.innerHTML = headerContent;
+    trigger.replaceWith(triggerButton);
+}
+
 const accordionTrg = document.querySelectorAll('.accordion-trigger');
 
 let i;
